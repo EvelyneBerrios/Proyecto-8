1ff0da1523faa6268df2fab759920db927bd0835
EvelyneBerrios <evelyne.berrios@hotmail.com>
2016-02-18 14:28:55 -0600

    Programa 8

---
+++ Proyecto 8/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+/*
+Ingresar la nota acumulada y examen, luego
+obtener la nota final y ajustarla a 60 si se
+encuentra 57-59 a 60 , presentar la nota
+y la estado si es aprobado o reprobado.
+*/
+
+int main()
+ nf= na+ne;
+if ((nf>=57)and (nf<=59))
+{nf=60;}
+    {cout << "Nota final es"<<nf<<"\n";
+    if(nf>=60)
+    {cout<<"aprobado\n";}
+    else
+    {cout<<"reprobado\n";}
+
+    if ((nota)and (nota<=59))
+    return 0;
+}
